b76fb42f070b92eeaf6b0239a1bb137e60076d5c
Luis Bacelar <lmbacelar@gmail.com>
2016-01-22 01:18:16 +0000

    added fuel policy

---
+++ app/policies/fuel_policy.rb
@@ -0,0 +1,25 @@
+class FuelPolicy < ApplicationPolicy
+  def index?
+    admin? || manager? || auditor?
+  end
+
+  def create?
+    admin? || manager?
+  end
+
+  def update?
+    admin? || manager?
+  end
+
+  def destroy?
+    admin? || manager?
+  end
+
+  def import?
+    admin? || manager?
+  end
+
+  def autocomplete?
+    admin? || manager? || auditor?
+  end
+end
