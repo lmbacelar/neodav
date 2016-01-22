ea25935613d4457d2d4b44923e54859b995783b9
Luis Bacelar <lmbacelar@gmail.com>
2016-01-22 02:02:04 +0000

    added vehicle types routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  resources :vehicle_types do
+    collection do
+      post :import
+    end
+  end
+
   resources :fuels do
     collection do
       post :import
