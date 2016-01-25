caa0d4999bfe12675051b0594fa12c7bea45395a
Luis Bacelar <lmbacelar@gmail.com>
2016-01-25 23:55:26 +0000

    added brand policy

---
+++ app/policies/brand_policy.rb
@@ -0,0 +1,25 @@
+class BrandPolicy < ApplicationPolicy
+  def index?
+    admin? || manager? || auditor?
+  end
+
+  def create?
+    admin? || manager?
+  end
+
+  def update?
+    admin? || manager?
+  end
+
+  def destroy?
+    admin? || manager?
+  end
+
+  def import?
+    admin? || manager?
+  end
+
+  def autocomplete?
+    admin? || manager? || auditor?
+  end
+end
