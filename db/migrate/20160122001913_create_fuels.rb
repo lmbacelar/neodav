4305f601e7c7dcfe38dc08a288311a611e59f8b0
Luis Bacelar <lmbacelar@gmail.com>
2016-01-25 22:20:46 +0000

    renamed name to code

--- db/migrate/20160122001913_create_fuels.rb
+++ db/migrate/20160122001913_create_fuels.rb
@@ -1,13 +1,13 @@
 class CreateFuels < ActiveRecord::Migration
   def change
     create_table :fuels do |t|
-      t.string :name,        null: false, default: ''
+      t.string :code,        null: false, default: ''
       t.string :description, null: false, default: ''
       t.string :slug,        null: false
 
       t.timestamps           null: false
 
-      t.index  :name,        unique: true
+      t.index  :code,        unique: true
       t.index  :slug,        unique: true
     end
   end
