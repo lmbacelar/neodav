7e43a7c8cee24966fb29bf637e1dc8313a187702
Luis Bacelar <lmbacelar@gmail.com>
2016-01-25 22:35:03 +0000

    schema updated

--- db/schema.rb
+++ db/schema.rb
@@ -30,14 +30,15 @@ ActiveRecord::Schema.define(version: 20160122012752) do
   add_index "friendly_id_slugs", ["sluggable_type"], name: "index_friendly_id_slugs_on_sluggable_type", using: :btree
 
   create_table "fuels", force: :cascade do |t|
-    t.string   "name",        default: "", null: false
+    t.string   "code",        default: "", null: false
     t.string   "description", default: "", null: false
     t.string   "slug",                     null: false
     t.datetime "created_at",               null: false
     t.datetime "updated_at",               null: false
   end
 
-  add_index "fuels", ["name"], name: "index_fuels_on_name", unique: true, using: :btree
+  add_index "fuels", ["code"], name: "index_fuels_on_code", unique: true, using: :btree
+  add_index "fuels", ["description"], name: "index_fuels_on_description", unique: true, using: :btree
   add_index "fuels", ["slug"], name: "index_fuels_on_slug", unique: true, using: :btree
 
   create_table "users", force: :cascade do |t|
@@ -63,14 +64,15 @@ ActiveRecord::Schema.define(version: 20160122012752) do
   add_index "users", ["slug"], name: "index_users_on_slug", unique: true, using: :btree
 
   create_table "vehicle_types", force: :cascade do |t|
-    t.string   "name",        default: "", null: false
+    t.string   "code",        default: "", null: false
     t.string   "description", default: "", null: false
     t.string   "slug",                     null: false
     t.datetime "created_at",               null: false
     t.datetime "updated_at",               null: false
   end
 
-  add_index "vehicle_types", ["name"], name: "index_vehicle_types_on_name", unique: true, using: :btree
+  add_index "vehicle_types", ["code"], name: "index_vehicle_types_on_code", unique: true, using: :btree
+  add_index "vehicle_types", ["description"], name: "index_vehicle_types_on_description", unique: true, using: :btree
   add_index "vehicle_types", ["slug"], name: "index_vehicle_types_on_slug", unique: true, using: :btree
 
 end
