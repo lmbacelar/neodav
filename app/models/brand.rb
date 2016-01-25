38f4d9a47d7fe022b122f7389fdeab726edb2b70
Luis Bacelar <lmbacelar@gmail.com>
2016-01-25 23:53:19 +0000

    added serializable, searchable, friendlyIded brand model

---
+++ app/models/brand.rb
@@ -0,0 +1,24 @@
+class Brand < ActiveRecord::Base
+  validates :code,        presence: true, uniqueness: true
+  validates :description, presence: true, uniqueness: true
+
+  default_scope { order :description }
+
+  include Serializable
+  io_attributes :code, :description
+
+  extend FriendlyId
+  friendly_id :code, use: :slugged
+
+  def self.search query
+    if query.present?
+      where "code = '#{query}' OR description ilike '%#{query}%'"
+    else
+      all
+    end
+  end
+
+  def to_s
+    code
+  end
+end
